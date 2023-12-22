4a8bb818bc91e832a47c7a7ab4f823b79bfd80d3
ahmad juma <juam17020@gmail.com>
2023-12-22 21:03:44 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+// Basic Math Functions
+
+// Addition
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Subtraction
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Multiplication
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+
+  function divide(a, b) {
+    return a / b;
+  }
+  
+
+  function increment(n) {
+    return n + 1;
+  }
+  
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
